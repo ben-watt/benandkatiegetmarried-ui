a6d1fee0dee6b58591e66a4a45f76f0a0b2c7bf7
Ben Watt <ben.watt@slicedbread.co.uk>
2017-08-24 22:10:59 +0100

    Added config options to navigation

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            loggedIn: cookieParser.cookieExists('_ncfa') ? true : false,
+            loggedIn: true,
         }
     }  
 
