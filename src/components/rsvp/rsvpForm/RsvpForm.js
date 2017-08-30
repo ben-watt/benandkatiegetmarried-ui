430732acdea7b8aee8d2f2bfff183681246d2458
kvizzle <katievallance1@gmail.com>
2017-08-30 13:10:51 +0100

    styles rsvp form

--- src/components/rsvp/rsvpForm/RsvpForm.js
+++ src/components/rsvp/rsvpForm/RsvpForm.js
@@ -5,6 +5,10 @@ class RsvpForm extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            guest: [{name: '', going: false, meal: ''}],
+        }
+        this.handleSubmit.bind(this);
     }
 
 
@@ -14,24 +18,36 @@ class RsvpForm extends React.Component {
 
     doThis = (val) => {
         return (
-           
-           
-                <tr key={val}>
-                    <td>{val}</td>
-                    <td><input type="radio" name={val} value="option2"  />I'll be there</td>
-                    <td> <input type="radio" name={val} value="option2"  />Cannot attend</td>
-                    <td> 
-                        <select>
-                            <option value="vegetarian">Vegetarian</option>
-                            <option value="meat">Meat</option>
-                            <option selected value="choose">Meal Choice</option>
-                        </select>
-                    </td>
-                </tr>
-               
-        
+            <div className={css.formElements} key={val} >
+                <h4 className={css.name}>{val}</h4>
+                <div className={css.input}><input type="radio" name={val} value="yes"  onClick={this.handleChange}/>I'll be there</div>
+                <div className={css.input}> <input type="radio" name={val} value="no"  onClick={this.handleChange}/>Cannot attend</div>
+                <select name={val}onChange={this.handleChange} >
+                    <option value="vegetarian">Vegetarian</option>
+                    <option value="meat">Meat</option>
+                    <option selected value="choose">Meal Choice</option>
+                </select>
+            </div>
         )
- 
+    }
+
+    handleChange = (event) => {
+        var field = event.target.name;
+        var value = event.target.value;
+        console.log(field);
+        console.log(value);
+        
+
+        this.setState((prevState) => {
+            // console.log(prevState.guest)
+            prevState.guest[field] = value  
+        });      
+  
+    }
+
+    handleSubmit = (event) => {
+        console.log(event.target);
+        event.preventDefault();
     }
 
     render() {            
@@ -39,9 +55,11 @@ class RsvpForm extends React.Component {
             <div className={[this.props.visibility && css.visible, css.notVisible].join(' ')}>
                 <p className={css.heading}> Will you attend? </p>
                 <p className={css.subHeading}>Please sign your RSVP</p>
-                <table className={css.names}>
-                    <tbody>{data.names.map(this.doThis)}</tbody>
-                </table>
+                
+                <form className={css.formOutline} onSubmit={this.handleSubmit} >
+                    {data.names.map(this.doThis)}
+                    <input type="submit" value="Submit" />
+                </form>
                 <p className={css.disclaimer}>*please note when making your meal choice, kosher catering will not be provided.  </p>
             </div>
         )
