153e7b70c772b596a823b0d3208fa6b0247f2175
Ben Watt <ben.watt@slicedbread.co.uk>
2017-11-18 19:09:49 +0000

    Change mouse to pointer on emoji hover

--- src/api/mockapi.js
+++ src/api/mockapi.js
@@ -7,7 +7,7 @@ const api = (function() {
     const api = axios.create({
         // 'https://benandkatiegetmarried.azurewebsites.net/api'
         // 'http://localhost:18399/api'
-        baseURL: 'http://localhost:18399/api',
+        baseURL: 'http://benandkatiegetmarried.co.uk',
         timeout: 8000,
         headers: {'X-Requested-With': 'XMLHttpRequest'}
     })
